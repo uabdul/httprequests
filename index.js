d04a5e01b33293d6092d6253d08142e16cff999f
Umair Abdul Qadir <umair.abdulq@gmail.com>
2018-12-03 21:17:10 +0000

    Completing HTTP request exercise.

---
+++ index.js
@@ -0,0 +1,11 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode, response.headers['content-type']);
+  })
+  .pipe(fs.createWriteStream('./future.jpg'));
